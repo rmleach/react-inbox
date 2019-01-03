ba326ff04a6e1873b0be368b290e92600228bdb1
Ryan Leach <ryanleach@Ryans-MacBook-Pro.local>
2019-01-03 11:42:59 -0700

    initial commit. Toolbar Component working

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import './App.css';
+import Toolbar from './Toolbar.js';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Toolbar></Toolbar>
+      </div>
+    );
+  }
+}
+
+export default App;
